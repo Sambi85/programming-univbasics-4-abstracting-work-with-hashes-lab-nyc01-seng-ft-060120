9a98dfd328dc47fc7637fab8049a814283ac986f
Samuel Lesser <ineedmoreplates@gmail.com>
2020-04-24 00:20:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -16,13 +16,12 @@ def update_counting_hash(hash, key)
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
   
-counter = 0
+if hash[key] == nil
 hash[key] = 1
-hash
 
-while counter < 5
+else
 hash[key] += 1
-counter +=1
   end
-  hash
+end
+hash
 end
