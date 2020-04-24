d0844e772008f531081acc3980dd3158dc90baea
Samuel Lesser <ineedmoreplates@gmail.com>
2020-04-24 00:14:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -17,11 +17,11 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   
 counter = 0
-hash[key] = counter
+hash[key] = 1
 
 while counter < 6
-var = hash[key] = counter
+hash[key] += 1
 counter +=1
   end
-var
+hash
 end
